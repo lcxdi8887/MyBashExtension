141a6ec219f4c17f76326eb8d24025ed8e8ad818
Ronan Liu <RonanLiu@ACYCAPITAL.local>
2018-05-28 11:06:09 +1000

    add initial extension file

---
+++ git-extension.bash
@@ -0,0 +1,13 @@
+# directly enter projects folder
+alias tocrm='cd c:/Users/ronanliu/zerologixProjects/project_crm'
+
+
+# git extensions
+# pull source dev to local
+alias gitplsd='git pull source dev'
+# git add all
+alias gitaa='git add .'
+# git commit with comment
+alias gitc='git commit -m'
+# git push to source's target branch
+alias gitpstb='git push source'
